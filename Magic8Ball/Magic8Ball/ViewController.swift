ddda51622b7a6cb0487f9dd324bd2f2e348a86e7
Treinamento <Treinamento@07HW012729s-MacBook-Pro.local>
2019-08-21 09:07:17 -0300

    first commit

---
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Treinamento on 14/08/19.
+//  Copyright © 2019 In.kpro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
